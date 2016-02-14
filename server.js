dfa6176d77b8b9008fdf8e7445f1bbe0dd4a2293
Joshua <tantohyung@gmail.com>
2016-02-14 11:05:58 +0800

    mongo uri to be obtained from config var

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var routes = require('./app/routes/index.js');
 
 var port = process.env.PORT || 5000;
 var app = express();
-var mongoUrl = 'mongodb://heroku_3sqdwhqq:bt2uc5rc50e746klblp498j68o@ds059185.mongolab.com:59185/heroku_3sqdwhqq';
+var mongoUrl = process.env.MONGOLAB_URI;
 
 mongoose.connect(mongoUrl);
 
